dbe371b829b3288c274fd84df205d90a5f1f5aac
Viсtoria Sukhenko <hy.rage@ya.ru>
2023-07-20 21:08:27 +0400

    npm updated

---
+++ index.js
@@ -0,0 +1,19 @@
+const { accountGen } = require('./accountGen');
+
+let i = 0;
+let exitCode,
+  success = 0,
+  fail = 0,
+  failDB = 0;
+let acCount = 2;
+async function main() {
+  while (i < acCount) {
+    exitCode = await accountGen();
+    exitCode == 0 ? success++ : exitCode == 1 ? fail++ : failDB++;
+    i++;
+  }
+  console.log(
+    `Запрошено ${acCount} аккаунтов\nУспешно создано\t\t\t - ${success}\nЗавершилось с ошибкой twitch\t - ${fail}\nЗавершилось с ошибкой бд\t - ${failDB}`
+  );
+}
+main();
